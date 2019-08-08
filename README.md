aafe55b3be25472f8ae26504cbcd411d24feb8b9
Krys112 <biomadkip@hotmail.co.uk>
2019-08-08 21:32:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Microposts
+
+Application which stores posts using a JSON server backend API and displays them in frontend with the functionality to edit and/or delete posts. Programmed in JavaScript ES6 to practice Asynchronous commands and working with APIs using fetch.
+
+This project also uses the Babel Webpack Starter by Brad Traversy; a starter pack to build JavaScript applications using standards from ES2015, ES2016 & ES2017. It uses webpack, Babel and webpack-dev-server to compile and serve. It is fully compatible with Async/Await as it uses the Babel polyfill.
+
+The initial commit is the base version of the project, created by following the 'Modern JavaScript From The Beginning' course by Brad Traversy available on Udemy: https://www.udemy.com/modern-javascript-from-the-beginning/
+
+### Installation
+
+Install the dependencies
+
+```sh
+$ npm install
+```
+
+### Serve
+To serve in the browser  - Runs webpack-dev-server
+
+```sh
+$ npm start
+```
+
+### Build
+Compile and build
+
+```sh
+$ npm run build
+```
+
+## More Info
+
+### Author
+
+Brad Traversy
+[Traversy Media](http://www.traversymedia.com)
+
+### License
+
+This project is licensed under the MIT License
---
+++ src/http.js
@@ -0,0 +1,110 @@
+class EasyHTTP {
+    //  // Make an HTTP GET Request
+    //  get(url) {
+    //    return new Promise((resolve, reject) => {
+    //      fetch(url)
+    //          .then(res => res.json())
+    //          .then(data => resolve(data))
+    //          .catch(err => reject(err));
+    //    });
+    //  }
+ 
+    //  // Make an HTTP POST Request
+    //  post(url, data) {
+    //    return new Promise((resolve, reject) => {
+    //      fetch(url, {
+    //         method: 'POST',
+    //         headers: {
+    //            'Content-type': 'application/json'
+    //         },
+    //         body: JSON.stringify(data)
+    //      })
+    //          .then(res => res.json())
+    //          .then(data => resolve(data))
+    //          .catch(err => reject(err));
+    //    });
+    //  }
+ 
+    //  // Make an HTTP PUT Request
+    //  put(url, data) {
+    //     return new Promise((resolve, reject) => {
+    //       fetch(url, {
+    //          method: 'PUT',
+    //          headers: {
+    //             'Content-type': 'application/json'
+    //          },
+    //          body: JSON.stringify(data)
+    //       })
+    //          .then(res => res.json())
+    //          .then(data => resolve(data))
+    //          .catch(err => reject(err));
+    //     });
+    //  }
+ 
+    //  // Make an HTTP DELETE Request
+    //  delete(url, data) {
+    //    return new Promise((resolve, reject) => {
+    //      fetch(url, {
+    //         method: 'DELETE',
+    //         headers: {
+    //            'Content-type': 'application/json'
+    //         }
+    //      })
+    //         .then(res => res.json())
+    //         .then(() => resolve('Resource Deleted...'))
+    //         .catch(err => reject(err));
+    //    });
+    // }
+ 
+    // ASYNC FROM HERE
+ 
+    // Make an HTTP GET Request
+    async get(url) {
+       const response = await fetch(url);
+       const resData = await response.json();
+       return resData;
+    }
+ 
+    async post(url, data) {
+       const response = await fetch(url, {
+          method: 'POST',
+          headers: {
+             'Content-type': 'application/json'
+          },
+          body: JSON.stringify(data)
+       });
+ 
+       const resData = await response.json();
+       return resData;
+    }
+ 
+    async put(url, data) {
+       const response = await fetch(url, {
+          method: 'PUT',
+          headers: {
+             'Content-type': 'application/json'
+          },
+          body: JSON.stringify(data)
+       });
+ 
+       const resData = await response.json();
+       return resData;
+    }
+ 
+    async delete(url, data) {
+       const response = await fetch(url, {
+          method: 'DELETE',
+          headers: {
+             'Content-type': 'application/json'
+          }
+       });
+ 
+       const resData = await 'Resource Deleted...';
+       return resData;
+    }
+ }
+
+ export const http = new EasyHTTP();
+ 
+ 
+ 
